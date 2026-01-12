f931df97d6e45a43e37b184e8a04890eb58d96d9
Suneet Maharana <suneetyoyo7@gmail.com>
2026-01-12 21:40:37 +0530

    .

--- src/rules/types.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class RuleType(Enum):
-    LIMIT = "LIMIT"                 # numeric thresholds
-    REQUIREMENT = "REQUIREMENT"     # must / mandatory
-    EXCEPTION = "EXCEPTION"         # overrides / approvals
-    OTHER = "OTHER"
