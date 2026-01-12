2865191e00bf1fe451d899fb071919e27e359d01
vasudharaje <vasudharaje03@gmail.com>
2026-01-12 23:45:11 +0530

    .

---
+++ frontend/routes.ts
@@ -0,0 +1,10 @@
+
+// This file can be used to define routes if utilizing a router like react-router-dom
+// Currently, the application is single-page, centered on Home.
+
+export const routes = [
+    {
+        path: '/',
+        component: 'Home'
+    }
+];
